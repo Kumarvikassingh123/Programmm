9f9a22ca184fafceaf45737d77d596d808bacc38
beaut <beaut@vikash>
2023-08-13 11:35:59 +0530

    my first commit

---
+++ substraction.java/src/substraction/java/substraction.java
@@ -0,0 +1,16 @@
+package substraction.java;
+
+public class substraction {
+	
+	public static void main(String[] args)
+	{
+		
+		int x = 56 ;
+		int y = 78 ;
+		int sub = y-x ;
+		System.out.println(sub);
+		
+		
+	}
+	
+}
